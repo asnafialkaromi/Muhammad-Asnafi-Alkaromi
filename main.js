df4ccd323efd6ede7cf85464cbb5edd0f4c311de
[asnafialkaromi] <[asnafialkaromi4@gmail.com]>
2023-10-07 22:12:29 +0700

    Complete the code

--- main.js
+++ main.js
@@ -1,56 +1,73 @@
 // If else
-
-if (condition) {
-    
+let auth = true;
+if (!auth) {
+    console.log("Anda Gagal Login");
 } else {
-    
+    console.log("Anda Berhasil Login")
 }
 
-// Nesterd If
-
-if (condition) {
-    
-} else if (condition){
-    
+// Nested If
+let gender = "Laki - Laki"
+if (gender == "Laki - Laki") {
+    console.log("Jenis Kelamin Anda " + gender)
+} else if (gender == "Perempuan"){
+    console.log("Jenis Kelamin Anda " + gender)
 } else {
-
+    console.log("Jenis Kelamin Anda tidak diketahui");
 }
 
 // Switch Case
-
-switch (key) {
-    case value:
-        
+let laptop = "Acer";
+switch (laptop) {
+    case "Asus":
+        console.log("Kamu Memilih Brand Laptop Asus")
         break;
-
+    case "Acer":
+        console.log("Kamu Memilih Brand Laptop Acer")
+        break;
+    case "Hp":
+        console.log("Kamu Memilih Brand Laptop Hp")
+        break;
+    case "Lenovo":
+        console.log("Kamu Memilih Brand Laptop Lenovo")
+        break;
+    case "MSI":
+        console.log("Kamu Memilih Brand Laptop MSI")
+        break;
     default:
+        console.log("Kamu Memilih Selain yang ada didaftar");
         break;
 }
 
 
 // For Statement
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+for (let num = 3; num > 0; num--) {
+    console.log("Hitung mundur dimulai dari : " + num);
 }
 
 // While 
 
-while (condition) {
-    
+let uang = 10000
+while (uang > 0) {
+    console.log("Kamu memiliki uang sebesar = Rp." + uang + ",-");
+    break;
 }
 
 // Do While
+let jarak = 10
 do{
-
-} while (condition) {
-    
+    console.log("Jarak sampai tujuan tersisa " + jarak + " KM");
+    jarak--;
+} while (jarak >= 0) {
+    console.log("Kamu sudah sampai tujuan");
 }
 
 
 // Function
 
-function name(params) {
-    
+function volumPersegi(a,b,c) {
+    return a * b * c;
 }
+
+console.log("Hasil dari perhitungan volume persegi = " + volumPersegi(3,5,2));
