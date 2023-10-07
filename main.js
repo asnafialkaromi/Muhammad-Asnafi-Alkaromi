295e5a78a98e9f9a1835960d28ea4da1cde7071b
[asnafialkaromi] <[asnafialkaromi4@gmail.com]>
2023-10-07 15:55:03 +0700

    Create stucture

---
+++ main.js
@@ -0,0 +1,56 @@
+// If else
+
+if (condition) {
+    
+} else {
+    
+}
+
+// Nesterd If
+
+if (condition) {
+    
+} else if (condition){
+    
+} else {
+
+}
+
+// Switch Case
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+
+// For Statement
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+// While 
+
+while (condition) {
+    
+}
+
+// Do While
+do{
+
+} while (condition) {
+    
+}
+
+
+// Function
+
+function name(params) {
+    
+}
